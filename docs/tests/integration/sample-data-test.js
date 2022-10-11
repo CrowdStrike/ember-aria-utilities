4ab578e3403dea8ed50ff0fd023c9fbd6c091b42
Nicole Chung <nicole.chung@crowdstrike.com>
2022-10-11 19:07:11 -0400

    fix: add test for sample-data component

---
+++ docs/tests/integration/sample-data-test.js
@@ -0,0 +1,47 @@
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+
+import { timeout } from 'ember-concurrency';
+
+module('Integration | Component | sample-data', function (hooks) {
+  setupRenderingTest(hooks);
+  test('it renders and displays data according to the timeout', async function (assert) {
+    await render(
+      hbs`<SampleData @columns={{2}} @rows={{2}} as |columns rows|>
+      <p data-test-selector="column">{{columns.[1]}}</p>
+      <p data-test-selector="total-columns">{{columns.length}}</p>
+      <p data-test-selector="row">{{rows.[1]}}</p>
+      <p data-test-selector="total-rows">{{rows.length}}</p>
+      </SampleData>`
+    );
+
+    // generateSampleData returns different text each time
+    assert.dom('[data-test-selector=column]').hasAnyText();
+    assert.dom('[data-test-selector=row]').hasAnyText();
+
+    assert.dom('[data-test-selector=total-columns]').hasText('2');
+    assert.dom('[data-test-selector=total-rows]').hasText('2');
+
+    await render(
+      hbs`<SampleData @columns={{4}} @rows={{4}} @timeout={{5000}} as |columns rows|>
+      <p data-test-selector="column">{{columns.[1]}}</p>
+      <p data-test-selector="total-columns">{{columns.length}}</p>
+      <p data-test-selector="row">{{rows.[1]}}</p>
+      <p data-test-selector="total-rows">{{rows.length}}</p>
+      </SampleData>`
+    );
+    assert.dom('[data-test-selector=column]').doesNotContainText();
+    assert.dom('[data-test-selector=row]').doesNotContainText();
+    assert.dom('[data-test-selector=total-columns]').doesNotContainText();
+    assert.dom('[data-test-selector=total-rows]').doesNotContainText();
+
+    await timeout(5000);
+
+    assert.dom('[data-test-selector=column]').hasAnyText();
+    assert.dom('[data-test-selector=row]').hasAnyText();
+    assert.dom('[data-test-selector=total-columns]').hasText('4');
+    assert.dom('[data-test-selector=total-rows]').hasText('4');
+  });
+});
