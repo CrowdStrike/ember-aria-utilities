7e051ae42b2452c531486fde4c20e584ea9a6b2a
Nicole Chung <nicole.chung@crowdstrike.com>
2022-10-11 17:02:56 -0400

    feat: make sample data async, but sync by default

--- docs/app/components/sample-data.js
+++ docs/app/components/sample-data.js
@@ -1,27 +1,42 @@
 import Component from '@glimmer/component';
-import { cached } from '@glimmer/tracking';
 import { setComponentTemplate } from '@ember/component';
 import { hbs } from 'ember-cli-htmlbars';
 
 import { generateSampleData } from 'docs/data';
+import { task, timeout } from 'ember-concurrency';
+import { taskFor } from 'ember-concurrency-ts';
+import { modifier } from 'ember-modifier';
 
 export default class SampleData extends Component {
-  @cached
-  get data() {
+  setup = modifier(() => {
+    taskFor(this._getData).perform();
+  });
+
+  @task
+  *_getData() {
+    const wait = this.args.timeout ?? 0;
+    // Simulate waiting for an async request
+    yield timeout(wait);
+
     return generateSampleData(this.args.rows, this.args.columns);
   }
 
+  get data() {
+    return taskFor(this._getData).lastSuccessful?.value;
+  }
+
   get columns() {
-    return this.data.headers;
+    return this.data?.headers;
   }
 
   get rows() {
-    return this.data.rows;
+    return this.data?.rows;
   }
 }
 
 setComponentTemplate(
   hbs`
+    {{this.setup}}
     {{yield this.columns this.rows}}
   `,
   SampleData
